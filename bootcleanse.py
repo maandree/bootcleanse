a9e1222c76e9ec74752602f19f30d25108d1795a
Mattias Andrée <maandree@operamail.com>
2012-12-28 07:58:21 +0100

    use gpl3
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- bootcleanse.py
+++ bootcleanse.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''
+bootcleanse – Cleanse your system from old MBR codes and boot flags
 
+Copyright © 2012  Mattias Andrée (maandree@kth.se)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 ## TODO  add support for bootable logical partitions
 
 import os
