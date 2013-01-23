bb3e641caeb5cc9c63b1c2fd90af10cea8736a6c
Mattias Andrée <maandree@operamail.com>
2013-01-23 21:20:46 +0100

    remove todo: nopony(?) actually have bootable(bootloader in) logical partitions.
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- bootcleanse.py
+++ bootcleanse.py
@@ -18,7 +18,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-## TODO  add support for bootable logical partitions
 
 import os
 import sys
